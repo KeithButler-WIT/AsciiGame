b8ed8e7f22b6ae738f6808f153301e5a9d241bf8
Keith Butler <20089137@mail.wit.ie>
2021-09-30 12:31:05 +0100

    Scrolls and range attacks

--- entity_factories.py
+++ entity_factories.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from components.ai import HostileEnemy
-from components.consumable import HealingConsumable
+from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
 from entity import Actor, Item
@@ -41,9 +41,27 @@ sam = Actor(
     inventory=Inventory(capacity=0),
 )
 
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
 health_potion = Item(
     char="!",
-    color=(127, 0, 255),
+    color=(0, 200, 0),
     name="Health Potion",
-    consumable=HealingConsumable(amount=4),
+    consumable=consumable.HealingConsumable(amount=4),
+)
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Lightning Scroll",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
